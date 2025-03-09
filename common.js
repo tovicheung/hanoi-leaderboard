6d5b2081ddf7a4789c3387166eb12101e48f94f2
tovicheung <discordpto1227@gmail.com>
2025-03-09 22:49:03 +0800

    clean

--- common.js
+++ common.js
@@ -3,13 +3,15 @@ function parseTime(millis) {
     return `${dur.getMinutes().toString().padStart(2, "0")}:${dur.getSeconds().toString().padStart(2, "0")}.${dur.getMilliseconds().toString().padStart(3, "0")}`
 }
 
-const id2titles = {
-    "leaderboard1": "4 disks",
-    "leaderboard2": "5 disks",
-}
-
-
 function updateLeaderboards() {
     updateLeaderboard("leaderboard1", leaderboard1);
     updateLeaderboard("leaderboard2", leaderboard2);
+}
+
+function min(a, b) {
+    return a < b ? a : b;
+}
+
+function max(a, b) {
+    return -min(-a, -b); // ha ha ha
 }
