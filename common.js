c33f8f98c0534868c2015e9826adc0f12966b665
tovicheung <discordpto1227@gmail.com>
2025-03-09 19:05:27 +0800

    changing lb

--- common.js
+++ common.js
@@ -8,35 +8,6 @@ const id2titles = {
     "leaderboard2": "5 disks",
 }
 
-function updateLeaderboard(id, leaderboard) {
-    const container = document.getElementById(id);
-    container.innerHTML = `<p>${id2titles[id]}</p>`;
-    var rank = 1;
-    for (const {name, score} of leaderboard) {
-        const record = document.createElement("div");
-        record.classList.add("record");
-        const recordRank = document.createElement("span");
-        recordRank.classList.add("record-rank");
-        recordRank.innerHTML = rank <= 3 ? ["&#129351;", "&#129352;", "&#129353;"][rank - 1] : rank.toString();
-        const recordName = document.createElement("span");
-        recordName.classList.add("record-name");
-        recordName.innerText = name;
-        const recordTime = document.createElement("span");
-        recordTime.classList.add("record-time");
-        recordTime.innerText = parseTime(score);
-        record.appendChild(recordRank);
-        record.appendChild(recordName);
-        record.appendChild(recordTime);
-        container.appendChild(record);
-
-        rank++;
-    }
-    if (leaderboard.length == 0) {
-        container.innerHTML = "<p>Waiting for challengers ...</p>";
-    }
-    container.style.backgroundColor = "lightgreen";
-    setTimeout(() => container.style.backgroundColor = "", 300);
-}
 
 function updateLeaderboards() {
     updateLeaderboard("leaderboard1", leaderboard1);
