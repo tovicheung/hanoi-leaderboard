efa18afddd427d372c01dc72b06fd59ab832693e
tovicheung <102522914+tovicheung@users.noreply.github.com>
2025-11-25 20:11:29 +0800

    last day changes

--- host.ps1
+++ host.ps1
@@ -1,2 +1,2 @@
 Write-Output "Hosting on $((Get-NetIPAddress -InterfaceAlias Wi-Fi).IPAddress):8000";
-deno run --unstable-kv --allow-net --allow-read --allow-env --env-file=.env main.ts
+deno run --unstable-kv --unstable-broadcast-channel --allow-net --allow-read --allow-env --env-file=.env main.ts
